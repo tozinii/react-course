dab59070e39d71a79bf335cccebd9bcc787308a4
Iker Apaolaza <iapaolaza@indaba.lks.es>
2023-09-07 13:25:51 +0200

    Formatting date object

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -3,7 +3,11 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div> {props.date.toISOString()} </div>
+      <div>
+        <div>{props.date.toLocaleString('es-ES', {month: 'long'})}</div>
+        <div>{props.date.getFullYear()}</div>
+        <div>{props.date.toLocaleString('es-ES', {day: '2-digit'})}</div>
+        </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
