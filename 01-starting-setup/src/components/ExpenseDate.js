f82953b5d269d74fed7afa045c6014c817fdd46d
Iker Apaolaza <iapaolaza@indaba.lks.es>
2023-09-07 13:40:43 +0200

    Splitting component into multiple components

---
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -0,0 +1,13 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(props) {
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{props.date.toLocaleString("es-ES", { month: "long" })}</div>
+      <div className="expense-date__year">{props.date.getFullYear()}</div>
+      <div className="expense-date__day">{props.date.toLocaleString("es-ES", { day: "2-digit" })}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>
-        <div>{props.date.toLocaleString('es-ES', {month: 'long'})}</div>
-        <div>{props.date.getFullYear()}</div>
-        <div>{props.date.toLocaleString('es-ES', {day: '2-digit'})}</div>
-        </div>
+        <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
