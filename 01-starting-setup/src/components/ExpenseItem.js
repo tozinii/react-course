586e4308a9490dfa94f28dc118d8fdaa152f89d1
Iker Apaolaza <iapaolaza@indaba.lks.es>
2023-09-07 12:34:11 +0200

    Outputting dynamic data and some JSX expressions

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 1, 14);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 249.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div> {expenseDate.toISOString()} </div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">249.67€</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}€</div>
       </div>
     </div>
   );
