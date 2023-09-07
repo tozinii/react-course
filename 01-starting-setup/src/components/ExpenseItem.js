e4a84787d20b12be826bf4c59dbc766d1ad89e21
Iker Apaolaza <iapaolaza@indaba.lks.es>
2023-09-07 12:19:33 +0200

    Complex JSX Code

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
-function ExpenseItem(){
-
-    return(
-        <h2>Expense item!</h2>
-    );
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <p>March 28th 2021</p>
+        <div>
+          <h2>Car Insurance</h2>
+          <p>249.67€</p>
+        </div>
+      </div>
+      <h2>Expense item!</h2>
+    </div>
+  );
 }
 
 export default ExpenseItem;
