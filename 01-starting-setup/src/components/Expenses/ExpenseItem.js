10778a225bafae65e3db929e5a47cc36a150d07e
tozinii <ikerapaolaza@gmail.com>
2023-09-10 01:14:50 +0200

    Listening to events

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,11 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+
+  const handleChangeTitleClick = (e) => {
+    console.log('Clicked!!');
+  }
+
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date} />
@@ -10,6 +15,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
       </div>
+      <button onClick={handleChangeTitleClick}>Change title</button>
     </Card>
   );
 }
