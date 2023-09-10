d9c4425623e7803b44eec5b87807858c32467708
tozinii <ikerapaolaza@gmail.com>
2023-09-10 11:51:48 +0200

    Changing variables values with state

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
+import React, { useState } from 'react';
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
 
+  let [title, setTitle] = useState(props.title);
+
   const handleChangeTitleClick = (e) => {
-    console.log('Clicked!!');
+    setTitle('Updated!');
   }
 
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
       </div>
       <button onClick={handleChangeTitleClick}>Change title</button>
