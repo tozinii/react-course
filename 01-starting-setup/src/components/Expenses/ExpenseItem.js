a772f5d6580bd7154981e86e22278204defb7070
tozinii <ikerapaolaza@gmail.com>
2023-10-25 19:40:39 +0200

    Created stateless component

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-
-  const [title, setTitle] = useState(props.title);
-
-  const handleChangeTitleClick = (e) => {
-    setTitle('Updated!');
-  }
-
   return (
     <Card className="expense-item">
-        <ExpenseDate date={props.date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
       </div>
-      <button onClick={handleChangeTitleClick}>Change title</button>
     </Card>
   );
 }
