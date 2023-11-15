6e0e9d0f12a77e7b2ab166c6f3ccd220ce0155ac
tozinii <ikerapaolaza@gmail.com>
2023-11-15 19:47:31 +0100

    adding inline styles

--- 02-styling-project/src/components/Header.jsx
+++ 02-styling-project/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logo from '../assets/logo.png';
-import './Header.css';
+import './Header.css'; // This import does not mean that the styles inside of it are only available on this component
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{color:'red','text-align':'left',backgroundColor:'black'}}>A community of artists and art-lovers.</p>
     </header>
   );
 }
