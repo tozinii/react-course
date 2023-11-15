ac4cb7463cb6448eafbdc1b8bd25f2dc0015ff9e
tozinii <ikerapaolaza@gmail.com>
2023-11-15 20:30:22 +0100

    adding css-modules

--- 02-styling-project/src/components/Header.jsx
+++ 02-styling-project/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logo from '../assets/logo.png';
-import './Header.css'; // This import does not mean that the styles inside of it are only available on this component
+import classes from './Header.module.css'; // This import does not mean that the styles inside of it are only available on this component
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
